f98cd35348ede832c8079a8597cc423f209bace6
jopatino <jopatino86@misena.edu.co>
2024-04-19 09:12:27 -0500

    Adding response type and itering user data

--- src/components/UsersPage.tsx
+++ src/components/UsersPage.tsx
@@ -1,16 +1,33 @@
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import axios from "axios";
+import type { ReqResUserListResponse, User } from "../interfaces";
+
+
+const loadUsers = async(): Promise<User[]> => {
+    try {
+        const { data } = await axios.get<ReqResUserListResponse>('https://reqres.in/api/users');
+        return data.data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+    
+    
+}
 
 export const UsersPage = () => {
 
+    const [users, setUsers] = useState<User[]>([])
+
     useEffect(() => {
+
+        loadUsers()
+            // .then( users => setUsers( users ) )
+            .then( setUsers )
 
         // fetch('https://reqres.in/api/users?page=2')
         //     .then(resp => resp.json())
         //     .then(data => console.log(data));
-
-        axios.get('https://reqres.in/api/users?page=2')
-            .then( resp => console.log( resp.data ) );
 
     }, [])
 
@@ -27,13 +44,34 @@ export const UsersPage = () => {
                 </tr>
                 </thead>
                 <tbody>
-                <tr>
-                    <td>avatar</td>
-                    <td>nombre</td>
-                    <td>email</td>
-                </tr>
+                    {
+                        users.map(user => (
+                            <UserRow key={ user.id } user={ user }/>
+                        ) )
+                    }
+                
                 </tbody>
             </table>
         </>
     )
 }
+
+interface Props {
+    user: User
+}
+
+
+const UserRow = ({ user }: Props) => {
+
+    const { avatar, first_name, last_name, email } = user;
+
+    return (
+        <tr>
+            <td><img style={{width: '50px'}} src={avatar} alt="User Avatar"/></td>
+            <td>{ `${first_name} ${last_name}` }</td>
+            <td>{email}</td>
+        </tr>
+    )
+}
+
+export default UsersPage
