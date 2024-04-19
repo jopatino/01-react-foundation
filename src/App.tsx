47e06d13331f4ec77467456914c4ec7837a73ec8
jopatino <jopatino86@misena.edu.co>
2024-04-18 19:19:34 -0500

    Literal objects (interfaces)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
-import {BasicTypes} from "./typescript";
+import {
+    // BasicTypes,
+    ObjectLiterals
+} from "./typescript";
 
 function App() {
 
   return (
     <main>
       <h1>Introducción a TS - React</h1>
-        <BasicTypes></BasicTypes>
+        {/*<BasicTypes></BasicTypes>*/}
+        <ObjectLiterals></ObjectLiterals>
     </main>
   )
 }
---
+++ src/typescript/ObjectLiterals.tsx
@@ -0,0 +1,39 @@
+
+interface Person {
+    firstName: string;
+    lastName: string;
+    age: number;
+    address: Address; /*Puedo anidar otras interfaces para organizar todo mejor*/
+    isAlive?: boolean; /*Opcional*/
+}
+
+interface Address {
+    country: string;
+    houseNo: number;
+}
+
+export const ObjectLiterals = () => {
+    
+    const person: Person = {
+        age: 27,
+        firstName: "Jose",
+        lastName: "Patiño",
+        isAlive: true,
+        address: {
+            country: 'Colombia',
+            houseNo: 123
+        }
+    }
+
+
+
+    return (
+        <>
+            <h3>Objetos literales</h3>
+            <pre>
+                { JSON.stringify( person, null, 2) }
+            </pre>
+
+        </>
+    )
+}
