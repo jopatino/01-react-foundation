209adaa52ce2f98827a7fa4eff180779cf144e9d
jopatino <jopatino86@misena.edu.co>
2024-04-22 20:50:16 -0500

    Added watch and onSubmit

--- src/components/FormsPage.tsx
+++ src/components/FormsPage.tsx
@@ -8,20 +8,25 @@ type FormInputs = {
 export const FormsPage = () => {
 
     // register registra los defaultvalues en el formulario, en los inputs
-    const { register } = useForm<FormInputs>({
+    const { register, handleSubmit, watch } = useForm<FormInputs>({
         defaultValues: {
             email: 'joseopatinoc@gmail.com',
             password: 'asb123'
         }
     });
 
+    const onSubmit = ( myForm: FormInputs ) => {
+        console.log({ myForm })
+    }
+
+    console.log(watch('email'));
+
     return (
         <>
-            <form>
-                
+            <form onSubmit={ handleSubmit( onSubmit ) }>
                 <h3>Formularios</h3>
                 <div style={{ display: 'flex', flexDirection: 'column' }}>
-                    <input type="text" placeholder="Email" { ...register('email') } />
+                    <input type="text" placeholder="Email" { ...register('email', {required: true}) } />
                     <input type="text" placeholder="password" { ...register('password') } />
                     <button type="submit">Ingresar</button>
                 </div>
